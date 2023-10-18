554f3535f7a0bd20766d4418d7dc93e1f9053ab9
Bhavin <patelbc94@gmail.com>
2023-10-18 14:34:13 +0530

    color

--- projects/colorChanger/colorChange.js
+++ projects/colorChanger/colorChange.js
@@ -5,7 +5,7 @@ buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
     //! Direct Without Condition Check
 
-    body.style.backgroundColor = e.target.id
+    body.style.backgroundColor = e.target.id;
 
     //!Using If Else Statement
     /* if (e.target.id === "grey") {
